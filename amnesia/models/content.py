b11a1f33369ba7230af2269e3e42dda02ea43e24
Julien Cigar <julien.cigar@gmail.com>
2013-04-25 10:29:05 +0200

    polymorphic loading improvements, code cleanup

--- amnesia/models/content.py
+++ amnesia/models/content.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import itertools
-
 from datetime import datetime
 
 import cherrypy
 
 from sqlalchemy import sql, orm
 from sqlalchemy.util import OrderedSet
+from sqlalchemy.types import Interval
 
 from amnesia import db
 
@@ -30,20 +29,13 @@ class Content(root.RootModel):
 
     query = db.Session.query_property(ContentQuery)
 
-    # Full text search columns
-    FTS = [('title', 'A'), ('description', 'B')]
-
     def __init__(self, **kwargs):
         super(Content, self).__init__(**kwargs)
 
-    def last_update(self, format=None):
-        # FIXME
-        format = "%Y-%m-%d at %H:%M"
-        if format is None:
-            format = config['timestamp_format']
+    def __repr__(self):
+        return "<{}, id={}>".format(self.__class__.__name__, self.id)
 
-        return self.updated.strftime(format) if self.updated else\
-                                                self.added.strftime(format)
+    __str__ = __repr__
 
     def url_for(self, action='show'):
         if action in ('show', 'index', None):
@@ -55,47 +47,6 @@ class Content(root.RootModel):
 
     def type_icon(self, size='16x16'):
         return cherrypy.url('/images/content_types/%s/%s' % (size, str(self.type.icon)))
-
-    def get_full_polymorphic_tree(self, polymorphic_map=None):
-        """Returns the full class hierarchy (in correct order) with a given 
-           list of polymorphic identities. The returned list can be used with
-           the with_polymorphic() Query's method."""
-
-        if not self.polymorphic_loading:
-            # TODO: check default polymorphic loading strategy (config ?)
-            return None
-        elif self.polymorphic_loading == 'n':
-            # No polymorphic loading
-            return None
-        elif self.polymorphic_loading == 'y':
-            # Polymorphic loading of _all_ mappers
-            return '*'
-        elif self.polymorphic_loading == 'c':
-            # Polymorphic loading of _some_ mappers. The polymorphic_identity
-            # is the content_type.id, so we have to retrieve all the
-            # primary mappers for those content_type.id. 
-            # This can be found in the .polymorphic_map property of the 
-            # root mapper (Content)
-            if not polymorphic_map:
-                polymorphic_map = orm.class_mapper(self.__class__).\
-                                  polymorphic_map
-
-            # At this time of writing, when you have more the one level of
-            # inheritance SQLAlchemy doesn't add the classes which don't
-            # have a polymorphic_identity .. so there is a missing JOIN.
-            # I use .iterate_to_root() to bypass the problem.
-            # Also, classes must be in the right order for the 
-            # with_polymorphic() function, otherwise SQLAlchemy joins
-            # tables in the wrong order. 
-            # I've added a ticket on the SQLAlchemy trac: 
-            # http://www.sqlalchemy.org/trac/ticket/1900
-            #
-            # TODO: could be replaced with .self_and_descendants (?)
-            return list(OrderedSet(itertools.chain(
-                *[reversed(list(polymorphic_map[identity].iterate_to_root()))\
-                for identity in [ct.id for ct in self.polymorphic_children]])))
-        else:
-            raise ValueError(self.polymorphic_loading)
 
     ###########
     # Filters #
@@ -132,6 +83,7 @@ class Content(root.RootModel):
     def filter_only_mine(cls):
         """Select items from a authenticated user"""
         try:
+            # FIXME config is undefined
             user_id = int(cherrypy.session[config['session_login_key']])
         except KeyError:
             pass
@@ -139,41 +91,28 @@ class Content(root.RootModel):
             return cls.owner_id == user_id
 
     @classmethod
-    def filter_date_trunc(cls, part, col=None):
-        """Helper function to extract the day or week or month from
-           Content.updated or Content.added (if Content.updated is NULL), and
-           compare this value to the current_date()"""
-        if not col: 
-            col = sql.func.coalesce(cls.updated, cls.added)
-        return sql.func.date_trunc(part, col) == \
-               sql.func.date_trunc(part, sql.func.current_date())
+    def filter_modified(cls, part, col=None):
+        if col is None:
+            col = cls.last_update
+
+        return sql.and_(col >= sql.func.date_trunc(part, sql.func.now()),
+                        col < sql.func.date_trunc(part, sql.func.now())\
+                              + sql.cast('1 %s' % part, Interval))
 
     @classmethod
     def filter_modified_today(cls):
         """Select items which have been modified today"""
-        return cls.filter_date_trunc('day')
+        return cls.filter_modified('day')
 
     @classmethod
     def filter_modified_week(cls):
         """Select items which have been modified this week"""
-        return cls.filter_date_trunc('week')
+        return cls.filter_modified('week')
 
     @classmethod
     def filter_modified_month(cls):
         """Select items which have been modified this month"""
-        return cls.filter_date_trunc('month')
-
-    @classmethod
-    def filter_perm_view(cls, human_obj=None):
-        """Select items which can be viewed by human_obj"""
-        if human_obj is not None:
-            return sql.or_(State.name == 'published',
-                           cls.owner_id == human_obj.id,
-                           human_obj.has_perm(('manager', 'view content')),
-                           sql.and_(State.name == 'pending',
-                                    human_obj.has_perm('change state')))
-        else:
-            return State.name == 'published'
+        return cls.filter_modified('month')
 
     @classmethod
     def filter_tag(cls, tag):
--- amnesia/models/folder.py
+++ amnesia/models/folder.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+from operator import itemgetter
+
 from sqlalchemy import orm
 
 from amnesia import db
@@ -19,20 +22,74 @@ class Folder(content.Content):
     def __init__(self, **kwargs):
         super(Folder, self).__init__(**kwargs)
 
+    def feed(self, **kwargs):
+        # Those columns must be loaded at first.
+        for c in ('polymorphic_loading', 'polymorphic_children'):
+            if c in kwargs:
+                setattr(self, c, kwargs.pop(c))
+        super(Folder, self).feed(**kwargs)
+
+    @property
+    def polymorphic_config(self):
+        """ Returns a namedtuple with the following:
+
+        pm : a filtered polymorphic map (if polymorphic_children is found) or an
+        empty dict (if polymorphic_loading if False) or the base mapper
+        polymorphic map (if polymorphic loading is True).
+
+        base : the base-most Mapper in the inheritance chain.
+
+        cls : classes which are found in the polymorphic map
+
+        orders : orders for those classes """
+
+        base_mapper = orm.object_mapper(self).base_mapper
+
+        # A "filtered" polymorphic_map which contain only mappers depending of
+        # the configuration of polymorphic_loading/polymorphic_children.
+        if self.polymorphic_loading:
+            pm = base_mapper.polymorphic_map
+            if self.polymorphic_children:
+                # Remove mappers from the original polymorphic_map for which
+                # identity is not found in polymorphic_children.
+                identities = {i.id for i in self.polymorphic_children}
+                pm = {k: v for (k, v) in pm.iteritems() if k in identities}
+        else:
+            # If no polymorphic loading, then _no_ mappers will be joined.
+            pm = {}
+
+        # Classes that should be automatically joined in the polymorphic
+        # loading. It can be directly used with orm.with_polymorphic()
+        cls = [m.class_ for m in pm.itervalues()]
+
+        # Fetch available orders. Note: we've to include the base-most Mapper
+        # class as it doesn't appear in the polymorphic map.
+        orders = {order for entity in cls + [base_mapper.class_]\
+                        for order in entity._SORTS_}
+
+        cfg = namedtuple('PolymorphicConfig', ('pm', 'base', 'cls', 'orders'))
+
+        return cfg(pm, base_mapper, cls, orders)
+
+    ##############
+    # VALIDATORS #
+    ##############
+
     @orm.validates('polymorphic_children', include_removes=True)
     def validate_polymorphic_children(self, key, value, is_remove):
         """ This validator maintains the consistency between default_order and
-        polymorphic children.
+        polymorphic_children.
 
         When an entry is removed from polymorphic_children (so, the underlying
         table will _not_ be joined anymore in the orm.with_polymorphic()),
         ensure that no default ordering has been specified on a column of that
-        table in default_order
-        """
+        table in default_order, otherwise we could end up with a missing table
+        in the final FROM clause """
 
         if is_remove and self.default_order:
-            to_remove = lambda x: x['polymorphic_identity'] != value.id
-            self.default_order = filter(to_remove, self.default_order)
+            self.default_order = filter(
+                lambda x: x['polymorphic_identity'] != value.id,
+                self.default_order)
 
         return value
 
@@ -40,57 +97,60 @@ class Folder(content.Content):
     def validate_default_order(self, key, value):
         """ Ensure consistency of the Folder.default_order column.
 
-        This function checks the value that will be serialized as default_order.
-        It must be a list of dict, each dict containing the following keys:
+        This function checks the value that will be serialized in
+        the default_order column. """
 
-        column : the column on which we plan to ORDER BY.
-        polymorphic_identity : used to retrieve the entity.
-        weight : position of the column in the ORDER BY clause.
-        order : 'asc' or 'desc'
+        orders = [order for order in value if\
+                  self.to_polymorphic_order(order) is not None]
 
-        Example:
+        # Stores the list sorted on the 'weight' column
+        orders.sort(key=itemgetter('weight'))
 
-        [{'column': 'starts', 'polymorphic_identity': 5, 'weight': 1,
-          'order': 'desc'},
+        return orders if orders else None
 
-         {'column': 'weight', 'polymorphic_identity': None, 'weight': 2,
-          'order': 'asc'},
+    def to_polymorphic_order(self, order):
+        """ Check a polymorphic order.
 
-         {'column': 'last_update', 'polymorphic_identity': None, 'weight': 3,
-          'order': 'asc'}]
+        The order must be a dict with the following keys:
 
-        Note: a polymorphic_identity of None correspond to the base-most Mapper in
-        the inheritance chain (Content in this case)
-        """
+        'key' : the column on which we plan to ORDER BY.
 
-        pl = self.polymorphic_loading
+        'polymorphic_identity' : used to retrieve the entity. Match all the
+         polymorphic_identity of the orm.mapper() in the models/__init__.py
+         file.
 
-        if pl is False or not value:
-            return None
-        elif pl is True:
-            keys = frozenset(('column', 'polymorphic_identity', 'weight', 'order'))
-            base_mapper = orm.object_mapper(self).base_mapper
-            pm = base_mapper.polymorphic_map
+        'weight' : position of the column in the ORDER BY clause.
 
-            # Identities that are supposed to be joined with orm.with_polymorphic
-            valid_identities = {i.id for i in self.polymorphic_children}
-            valid_orders = []
+        'order' : 'asc' or 'desc' (sort direction)
 
-            for order in value:
-                # All keys must be present
-                if frozenset(order) == keys:
-                    identity = order['polymorphic_identity']
+        Examples:
+        =========
 
-                    # If polymorphic loading is enabled, check that the
-                    # 'polymorphic_identity' is valid (which means that the
-                    # identity is present in polymorphic_children), that the
-                    # column exists, and that we're allowed to ORDER BY on that
-                    # column.
-                    if identity in valid_identities or identity is None:
-                        cls = pm.get(identity, base_mapper).class_
-                        col = getattr(cls, order['column'], None)
+        {'key': 'starts', 'polymorphic_identity': 5, 'weight': 1,
+         'order': 'desc'}
 
-                        if col is not None and col in cls.__order__:
-                            valid_orders.append(order)
+        {'key': 'weight', 'polymorphic_identity': None, 'weight': 2,
+         'order': 'asc'}
 
-            return valid_orders if valid_orders else None
+        {'key': 'last_update', 'polymorphic_identity': None, 'weight': 3,
+         'order': 'asc'}
+
+        Note:
+        =====
+
+        A polymorphic_identity of None correspond to the base-most Mapper in
+        the inheritance chain (Content in this case) """
+
+        keys = frozenset(('key', 'polymorphic_identity', 'weight', 'order'))
+
+        if frozenset(order) == keys:
+            pl_cfg = self.polymorphic_config
+            identity = order['polymorphic_identity']
+            if identity in pl_cfg.pm or identity is None:
+                cls = pl_cfg.pm.get(identity, pl_cfg.base).class_
+                col = getattr(cls, order['key'], None)
+
+                if col is not None and col in cls._SORTS_:
+                    return col.desc() if order['order'] == 'desc' else col
+
+        return None
