67fb1ed8030da5322291a5d9410822d5b0c892ec
Julien Cigar <julien.cigar@gmail.com>
2015-10-14 23:33:08 +0200

    more strict on pyramid version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid',
+    'pyramid>1.5,<1.6',
     'pyramid_chameleon',
 #    'pyramid_debugtoolbar',
     'pyramid_tm',
