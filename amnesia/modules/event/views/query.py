9ca89cb1fa8d1bd7719037df0905210ed64c597c
Julien Cigar <julien@perdition.city>
2018-01-04 11:18:25 +0100

    remove BBPF specific codeé

--- amnesia/modules/event/views/query.py
+++ amnesia/modules/event/views/query.py
@@ -6,24 +6,7 @@ from sqlalchemy import sql
 
 from pyramid.view import view_config
 
-from amnesia.modules.event import Event
-from amnesia.modules.event import EventResource
-
 
 def includeme(config):
     ''' Pyramid includeme func'''
     config.scan(__name__)
-
-
-@view_config(request_method='GET', context=EventResource, name='sliderhome',
-             renderer='amnesia:templates/event/view/_slider_home.pt')
-def sliderhome(context, request):
-    filters = sql.and_(
-        Event.filter_published(),
-        sql.not_(Event.filter_finished())
-    )
-
-    query = context.query()
-    query = query.filter(filters).order_by(Event.starts).limit(10)
-
-    return {'events': query.all()}
