019194ba43320177a388bcad5648c25cc166fd0f
Julien Cigar <julien@perdition.city>
2018-01-04 11:09:07 +0100

    remove bbpf specific code

--- amnesia/modules/document/views/query.py
+++ amnesia/modules/document/views/query.py
@@ -6,34 +6,7 @@ from sqlalchemy import sql
 
 from pyramid.view import view_config
 
-from amnesia.modules.document import Document
-from amnesia.modules.document import DocumentResource
-
 
 def includeme(config):
     ''' Pyramid includeme func'''
     config.scan(__name__)
-
-
-@view_config(request_method='GET', context=DocumentResource, name='newshome',
-             renderer='amnesia:templates/document/view/_news_home.pt')
-def newshome(context, request):
-    news_container = request.registry.settings.get('news_container_id')
-
-    if not news_container:
-        documents = []
-    else:
-        filters = sql.and_(
-            Document.filter_published(),
-            Document.filter_container_id(news_container)
-        )
-
-        query = context.query()
-        query = query.filter(filters).order_by(Document.added.desc()).limit(5)
-
-        documents = query.all()
-
-    return {
-        'documents': documents,
-        'news_container': news_container
-    }
