27041327eefacda5a572881cc99f072453042eaa
Julien Cigar <julien@perdition.city>
2018-08-07 14:38:26 +0200

    revert #name, (see https://github.com/Pylons/pyramid-cookiecutter-alchemy/pull/44#issuecomment-411039678)

--- amnesia/scripts/initializemime.py
+++ amnesia/scripts/initializemime.py
@@ -45,9 +45,8 @@ def main(argv=sys.argv):
 
     config_uri = argv[1]
     options = parse_vars(argv[2:])
-    name = options.pop('name', None)
     setup_logging(config_uri)
-    settings = get_appsettings(config_uri, name=name, options=options)
+    settings = get_appsettings(config_uri, options=options)
     config = Configurator(settings=settings)
     config.include('amnesia.db')
     config.include('amnesia.modules.mime.mapper')
