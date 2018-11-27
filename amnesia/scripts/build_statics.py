6ac74e03bf04a82dcc06d0eff16484e7e22fe0b1
Julien Cigar <julien@perdition.city>
2018-11-27 15:11:29 +0100

    add assets build script

--- amnesia/scripts/build_statics.py
+++ amnesia/scripts/build_statics.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import logging
-
+import argparse
 import os
-import shutil
 import sys
 
 from pyramid.scripts.common import parse_vars
@@ -11,32 +9,27 @@ from pyramid.paster import setup_logging
 from pyramid.paster import bootstrap
 
 
-def usage(argv):
-    cmd = os.path.basename(argv[0])
-    print('usage: {0} <config_uri> [var=value]\n'
-          '(example: "{0} development.ini")'.format(cmd))
-    sys.exit(1)
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_uri",
+                        help="Configuration file, e.g., development.ini")
+    return parser.parse_args(argv[1:])
 
 
 def main(argv=sys.argv):
+    args = parse_args(argv)
+    setup_logging(args.config_uri)
+    env = bootstrap(args.config_uri)
+    request = env['request']
+    registry = request.registry
 
-    if len(argv) < 2:
-        usage(argv)
+    if not hasattr(registry, 'cms_frontend_assets'):
+        print('===>>> No static packages registered')
+        return
 
-    config_uri = argv[1]
-    options = parse_vars(argv[2:])
-    setup_logging(config_uri)
+    assets = registry.cms_frontend_assets
 
-    with bootstrap(config_uri, options=options) as env:
-        registry = env['registry']
-
-        if not hasattr(registry, 'cms_frontend_assets'):
-            print('===>>> No static packages registered')
-            return
-
-        assets = registry.cms_frontend_assets
-
-        for (plugin_name, config) in assets.items():
-            if config['build_script']:
-                print('===>>> Running build script for {}'.format(plugin_name))
-                config['build_script'](registry, config)
+    for (plugin_name, config) in assets.items():
+        if config['build_script']:
+            print('===>>> Running build script for {}'.format(plugin_name))
+            config['build_script'](registry, config)
