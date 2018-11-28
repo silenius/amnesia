62a5bc4a9c24af77c8cc9710129cd08831179b53
Julien Cigar <julien@perdition.city>
2018-11-28 11:01:44 +0100

    chmod -R static dir after build

--- amnesia/scripts/build_statics.py
+++ amnesia/scripts/build_statics.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import os.path
 import sys
 
 from pyramid.scripts.common import parse_vars
@@ -12,8 +13,16 @@ from pyramid.paster import bootstrap
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("config_uri",
-                        help="Configuration file, e.g., development.ini")
+                        help="configuration file, e.g., development.ini")
     return parser.parse_args(argv[1:])
+
+
+def fix_permissions(src, dir_mode=0o755, file_mode=0o644):
+    for root, dirs, files in os.walk(src):
+        for item in dirs:
+            os.chmod(os.path.join(root, item), dir_mode)
+        for item in files:
+            os.chmod(os.path.join(root, item), file_mode)
 
 
 def main(argv=sys.argv):
@@ -22,6 +31,8 @@ def main(argv=sys.argv):
     env = bootstrap(args.config_uri)
     request = env['request']
     registry = request.registry
+
+    build_dir = registry.settings['static.dir']
 
     if not hasattr(registry, 'cms_frontend_assets'):
         print('===>>> No static packages registered')
@@ -33,3 +44,6 @@ def main(argv=sys.argv):
         if config['build_script']:
             print('===>>> Running build script for {}'.format(plugin_name))
             config['build_script'](registry, config)
+
+    print('===>>> Fixing permissions of {}'.format(build_dir))
+    fix_permissions(build_dir)
