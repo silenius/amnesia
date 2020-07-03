3d68fbead7c39b8a72230a4ec149aa6de62ba979
Julien Cigar <julien@perdition.city>
2020-07-03 17:16:23 +0200

    add missing file

---
+++ amnesia/modules/folder/events.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class RAddToFolder:
+
+    def __init__(self, request, folder, content):
+        self.request = request
+        self.folder = folder
+        self.entity = content
