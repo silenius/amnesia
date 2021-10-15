851efa7b10c162df87d0d130123379c26f91b2f7
Julien Cigar <julien@perdition.city>
2021-10-15 16:06:20 +0200

    add Owner role

--- amnesia/alembic/sql/upgrade/20211015_a31f35e95704_remove__content.sql
+++ amnesia/alembic/sql/upgrade/20211015_a31f35e95704_remove__content.sql
@@ -1,1 +1,2 @@
 UPDATE permission SET name=split_part(name, '_content', 1);
+INSERT INTO role(name, enabled, locked, description) VALUES ('system.Owner', true, true, 'Automatically granted to the creator of an object.');
