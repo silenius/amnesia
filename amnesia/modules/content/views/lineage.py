cc875e0237375de1d824ba0af8dd983eb8cb79ae
Julien Cigar <julien@perdition.city>
2023-09-26 11:00:37 +0200

    typing

--- amnesia/modules/content/views/lineage.py
+++ amnesia/modules/content/views/lineage.py
@@ -1,5 +1,7 @@
 import logging
 
+from typing import TypedDict
+
 from pyramid.view import (
     view_defaults,
     view_config
@@ -9,12 +11,16 @@ from ..services import get_lineage
 from .. import Entity
 from amnesia.views import BaseView
 
-
 log = logging.getLogger(__name__)
 
 
 def includeme(config):
     config.scan(__name__)
+
+
+class LineageInfo(TypedDict):
+    id: int
+    title: str
 
 
 @view_defaults(
@@ -27,13 +33,13 @@ class Lineage(BaseView):
         request_method='GET',
         renderer='json'
     )
-    def get(self):
+    def get(self) -> list[LineageInfo]:
         stmt = get_lineage(self.context.entity)
         tree = self.dbsession.scalars(stmt).all()
         tree.append(self.context.entity)
 
         return [{
-            'id': folder.id,
-            'title': folder.title
-        } for folder in tree ]
+            'id': content.id,
+            'title': content.title
+        } for content in tree]
         
