52836146dfc43111e2bd996b86b526ee4115f1ab
Julien Cigar <julien@perdition.city>
2023-09-26 09:53:01 +0200

    remove now unused files

--- amnesia/modules/document/forms.py
+++
@@ -1,8 +0,0 @@
-from amnesia.modules.content.forms import ContentForm
-
-class DocumentForm(ContentForm):
-
-    template = 'amnesia:templates/document/_form.pt'
-
-    def __init__(self, request, template=None):
-        super().__init__(request, template)
