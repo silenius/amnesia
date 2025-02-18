77db556d6dff814cc8ad2d239fbe5e8bd542497c
Julien Cigar <julien@perdition.city>
2025-02-18 16:15:02 +0100

    add HttpHeaderCSRFStoragePolicy

--- amnesia/security/policy.py
+++ amnesia/security/policy.py
@@ -1,13 +1,22 @@
+import secrets
 import logging
 
 from pyramid.authentication import AuthTktCookieHelper
 from pyramid.authorization import ACLHelper
 from pyramid.authorization import Authenticated
 from pyramid.authorization import Everyone
+from pyramid.interfaces import ICSRFStoragePolicy
 from pyramid.request import RequestLocalCache
 from pyramid.settings import asbool
+from pyramid.util import (
+    bytes_,
+    text_,
+    strings_differ,
+)
 
 from sqlalchemy import sql
+
+from zope.interface import implementer
 
 
 __all__ = ['cookie_security_policy']
@@ -98,3 +107,51 @@ class AmnesiaSecurityPolicy:
                 principals.update(context_principals)
 
         return list(principals)
+
+
+
+@implementer(ICSRFStoragePolicy)
+class HttpHeaderCSRFStoragePolicy:
+    """A CSRF storage policy that persists the CSRF token in an HTTP header.
+
+    ``header_name``
+
+        The header name here the CSRF token will be stored.
+        Default: `X-CSRF-Token`.
+
+    """
+
+    _token_factory = staticmethod(lambda: text_(secrets.token_hex()))
+
+    def __init__(self, header_name='X-CSRF-Token'):
+        self.header_name = header_name
+
+    def new_csrf_token(self, request):
+        """Sets a new CSRF token into the header and returns it."""
+        token = self._token_factory()
+        request.headers[self.header_name] = token
+
+        def set_header(request, response):
+            response.headers.add(self.header_name, token)
+
+        request.add_response_callback(set_header)
+        
+        return token
+
+    def get_csrf_token(self, request):
+        """Returns the currently active CSRF token from the header,
+        generating a new one if needed."""
+        token = request.headers.get(self.header_name)
+
+        if not token:
+            token = self.new_csrf_token(request)
+        
+        return token
+
+    def check_csrf_token(self, request, supplied_token):
+        """Returns ``True`` if the ``supplied_token`` is valid."""
+        expected_token = self.get_csrf_token(request)
+
+        return not strings_differ(
+            bytes_(expected_token), bytes_(supplied_token)
+        )
